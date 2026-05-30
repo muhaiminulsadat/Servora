f8262db821c61def9ba71618752df2ea4b4f1db6
Sadat <muhaiminulsadat@gmail.com>
2026-05-31 05:27:39 +0600

    feat(mail-service): initialize mail service with express and nodemailer
    - Add package.json for mail service with necessary dependencies
    - Create app.ts to set up express application
    - Implement mail configuration in mail.config.ts using nodemailer
    - Add mail controller to handle sending emails with validation
    - Define mail routes in mail.route.ts for sending emails
    - Create server.ts to start the express server
    - Implement mail service logic in mail.service.ts for sending emails
    - Define ApiResponse type for consistent API responses
    - Create AppError class for handling application errors
    - Add TypeScript configuration in tsconfig.json for the mail service
    
    feat(frontend): add button component and utility functions
    
    - Create components.json for component configuration
    - Implement reusable button component with variants and styles
    - Add utility function for class name merging in utils.ts

---
+++ backend/auth-service/src/config/redis.config.ts
@@ -0,0 +1,27 @@
+import {createClient, RedisClientType} from "redis";
+
+let client: RedisClientType;
+
+export const connectRedis = async () => {
+  client = createClient({
+    username: "default",
+    password: process.env.REDIS_PASSWORD!,
+    socket: {
+      host: process.env.REDIS_HOST!,
+      port: parseInt(process.env.REDIS_PORT!),
+    },
+  });
+
+  client.on("error", (err) => console.log("Redis Client Error", err));
+
+  await client.connect();
+
+  console.log("Connected to Redis");
+};
+
+export const getRedisClient = (): RedisClientType => {
+  if (!client) {
+    throw new Error("Redis client is not connected");
+  }
+  return client;
+};
